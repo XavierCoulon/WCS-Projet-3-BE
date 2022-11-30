ae4a3e551804c9644a859a4b0501b4b732fbd964
JIdayyy <julien_abbadie@hotmail.fr>
2022-11-30 11:05:28 +0100

    init

---
+++ src/api/books/interface.ts
@@ -0,0 +1,11 @@
+import { RequestHandler } from "express";
+
+type Book = {
+  id: string;
+  title: string;
+  year: Date;
+};
+
+export interface BookHandlers {
+  getAll: RequestHandler<Record<any, string>, Book[], null>;
+}
