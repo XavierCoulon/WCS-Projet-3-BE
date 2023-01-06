fd5a547cf67a3a424e077005d4d1f9bea9d1b6e3
Pupuce-64 <laeti.decandt@gmail.com>
2023-01-06 16:10:19 +0100

    modified the create video handler so it is able to threat videos upload

--- src/api/videos/handlers/create.ts
+++ src/api/videos/handlers/create.ts
@@ -1,34 +1,69 @@
 /* eslint-disable no-console */
 import { VideoHandlers } from "../interface";
 import prisma from "../../../../prisma/client";
+import busboy from "busboy";
+import { PassThrough, Stream } from "stream";
+import uploadToS3 from "../../../utils/uploadToMinio";
 
 const createVideo: VideoHandlers["create"] = async (req, res) => {
   try {
-    const {
-      categoryId,
-      description,
-      display,
-      duration,
-      isPublic,
-      teaserUrl,
-      thumbnailUrl,
-      title,
-      videoUrl,
-    } = req.body;
-    const videoToCreate = await prisma.video.create({
-      data: {
-        categoryId,
-        description,
-        display,
-        duration,
-        isPublic,
-        teaserUrl,
-        videoUrl,
-        thumbnailUrl,
-        title,
-      },
+    const bb = busboy({ headers: req.headers });
+    const formaDataFields = {
+      categoryId: "",
+      description: "",
+      teaserUrl: "",
+      thumbnailUrl: "",
+      title: "",
+    };
+
+    bb.on("field", (fieldname: keyof typeof formaDataFields, val: string) => {
+      formaDataFields[fieldname] = val;
     });
-    res.status(200).json(videoToCreate);
+
+    bb.on(
+      "file",
+      async (
+        fieldname: string,
+        file: Stream,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        fileData: Record<string, any>
+      ) => {
+        if (fieldname !== "files") {
+          return res
+            .status(400)
+            .send({ message: "Invalid fieldname. Try with [files] instead" });
+        }
+
+        console.log("Upload started");
+
+        const passThrough = new PassThrough();
+        file.pipe(passThrough);
+
+        const metadata = {
+          "Content-type": fileData.mimeType,
+        };
+
+        await uploadToS3(passThrough, `/videos/${fileData.filename}`, metadata);
+
+        const videoToCreate = await prisma.video.create({
+          data: {
+            ...formaDataFields,
+            display: true,
+            isPublic: true,
+            duration: 0,
+            videoUrl: `https://${process.env.MINIO_ENDPOINT}/origin/videos/${fileData.filename}`,
+          },
+        });
+
+        res.status(200).json(videoToCreate);
+      }
+    );
+
+    bb.on("finish", () => {
+      console.log("FINISHED");
+    });
+
+    return req.pipe(bb);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error });
--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = Express();
 import cors from "cors";
 // Add a list of allowed origins.
 // If you have more origins you would like to add, you can add them to the array below.
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["*"];
 
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
