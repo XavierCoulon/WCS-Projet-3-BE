3fec0c72de1cd7e571d5965bdc111a531b4b5dab
Pupuce-64 <laeti.decandt@gmail.com>
2023-01-10 11:41:22 +0100

    fixed the allowed origins cors

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = Express();
 import cors from "cors";
 // Add a list of allowed origins.
 // If you have more origins you would like to add, you can add them to the array below.
-const allowedOrigins = ["*"];
+const allowedOrigins = ["http://localhost:3000"];
 
 app.use(
   cors({
