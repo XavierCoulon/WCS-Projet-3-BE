2a000444610a3fb5733c4c51ce07ff25e5615b3d
Nicolas-CARRERE <nicolascarrere.pro@gmail.com>
2023-01-16 10:32:46 +0100

    commit before pull dev

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = Express();
 import cors from "cors";
 // Add a list of allowed origins.
 // If you have more origins you would like to add, you can add them to the array below.
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
 
 app.use(
   cors({
