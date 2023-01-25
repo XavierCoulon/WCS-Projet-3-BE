081bdef75a28bb239f9f7ecdb6e634086cd694c6
XavierCoulon <xcoulon.mail@gmail.com>
2023-01-25 14:17:20 +0100

    remove guest role + play with prisma.take for limit on favorites

--- src/api/videos/handlers/getAll.ts
+++ src/api/videos/handlers/getAll.ts
@@ -3,58 +3,36 @@ import { VideoHandlers } from "../interface";
 import prisma from "../../../../prisma/client";
 
 const getAllVideos: VideoHandlers["getAll"] = async (req, res) => {
-  // const { role } = req.user;
   const { favorite } = req.query;
   console.log(req.query);
 
-  // const isGuest = role === "GUEST";
   try {
-    const videos =
-      favorite === "true"
-        ? await prisma.video.findMany({
-            select: {
-              id: true,
-              display: true,
-              createdAt: true,
-              categoryId: true,
-              description: true,
-              isPublic: true,
-              videoUrl: true, // replace by true for testing back-office links
-              duration: true,
-              nbViews: true,
-              teaserUrl: true,
-              thumbnailUrl: true,
-              title: true,
-              updatedAt: true,
+    const videos = await prisma.video.findMany({
+      select: {
+        id: true,
+        display: true,
+        createdAt: true,
+        categoryId: true,
+        description: true,
+        isPublic: true,
+        videoUrl: true,
+        duration: true,
+        nbViews: true,
+        teaserUrl: true,
+        thumbnailUrl: true,
+        title: true,
+        updatedAt: true,
+      },
+      where: {
+        users_favorites: {
+          some: {
+            id: {
+              contains: favorite === "true" ? req.user.id : "",
             },
-            where: {
-              users_favorites: {
-                some: {
-                  id: {
-                    contains: favorite === "true" ? req.user.id : "",
-                  },
-                },
-              },
-            },
-          })
-        : await prisma.video.findMany({
-            select: {
-              id: true,
-              display: true,
-              createdAt: true,
-              categoryId: true,
-              description: true,
-              isPublic: true,
-              videoUrl: true, // replace by true for testing back-office links
-              duration: true,
-              nbViews: true,
-              teaserUrl: true,
-              thumbnailUrl: true,
-              title: true,
-              updatedAt: true,
-            },
-          });
-
+          },
+        },
+      },
+    });
     res.status(200).json(videos);
   } catch (error) {
     console.log(error);
