24b4714c344e04a903b07de14b7a27d4beeacede
Nicolas-CARRERE <nicolascarrere.pro@gmail.com>
2023-02-07 16:47:46 +0100

    FIX/ page update road is now ok. page uuid staying

--- src/api/page/handlers/createPageWithSections.ts
+++ src/api/page/handlers/createPageWithSections.ts
@@ -13,6 +13,8 @@ const createPageWithSections: PageHandlers["createWithSections"] = async (
       pagesSectionsDynamicData,
       pagesAdvertisingsData,
     } = req.body;
+    console.log(req.body);
+
     const newPage = await prisma.page.create({
       data: {
         title,
--- src/api/page/handlers/update.ts
+++ src/api/page/handlers/update.ts
@@ -3,17 +3,91 @@ import prisma from "../../../../prisma/client";
 
 const updatePage: PageHandlers["update"] = async (req, res) => {
   try {
-    const { id } = req.params;
-    const { title } = req.body;
-    const page = await prisma.page.update({
+    const {
+      title,
+      display,
+      pagesSectionsStaticData,
+      pagesSectionsDynamicData,
+      pagesAdvertisingsData,
+    } = req.body;
+    const pageId = req.params.id;
+    // First, get elements to disconnect
+    const page = await prisma.page.findUnique({
       where: {
-        id: id,
+        id: pageId,
+      },
+      include: {
+        pagesSectionsStatic: true,
+        pagesSectionsDynamic: true,
+        pagesAdvertisings: true,
+      },
+    });
+
+    // Then, disconnect elements
+    await prisma.page.update({
+      where: {
+        id: pageId,
+      },
+      data: {
+        pagesSectionsStatic: {
+          delete: page?.pagesSectionsStatic?.map((section) => ({
+            id: section.id,
+          })),
+        },
+        pagesSectionsDynamic: {
+          delete: page?.pagesSectionsDynamic?.map((section) => ({
+            id: section.id,
+          })),
+        },
+        pagesAdvertisings: {
+          delete: page?.pagesAdvertisings?.map((section) => ({
+            id: section.id,
+          })),
+        },
+      },
+    });
+
+    // Then, connect new advertisings to this page
+    const newUpdatedPage = await prisma.page.update({
+      where: {
+        id: pageId,
       },
       data: {
         title,
+        display,
+        pagesSectionsStatic: {
+          create: pagesSectionsStaticData?.map((section) => ({
+            position: section.position,
+            sectionsStatics: {
+              connect: {
+                id: section.id,
+              },
+            },
+          })),
+        },
+        pagesSectionsDynamic: {
+          create: pagesSectionsDynamicData?.map((section) => ({
+            position: section.position,
+            sectionsDynamic: {
+              connect: {
+                id: section.id,
+              },
+            },
+          })),
+        },
+        pagesAdvertisings: {
+          create: pagesAdvertisingsData?.map((section) => ({
+            position: section.position,
+            advertisings: {
+              connect: {
+                id: section.id,
+              },
+            },
+          })),
+        },
       },
     });
-    res.status(201).json(page);
+    res.status(201).json(newUpdatedPage);
   } catch (error) {
     res.status(500).json({ message: error });
   }
