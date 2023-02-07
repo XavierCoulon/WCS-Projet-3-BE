4215fc16d11602225eb720624ae643ced8be4585
JIdayyy <julien_abbadie@hotmail.fr>
2023-02-07 10:07:50 +0100

    log origin cors

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const whiteListedUrls = process.env.CORS_WHITELISTED_URLS?.split(",") || [];
+// const whiteListedUrls = process.env.CORS_WHITELISTED_URLS?.split(",") || [];
 
 export const corsOptions = {
   origin: (
@@ -11,19 +11,20 @@ export const corsOptions = {
     callback: (err: Error | null, allow?: boolean) => Error | void
   ): void | Error => {
     console.log("origin", origin);
-    if (process.env.NODE_ENV === "development") {
-      return callback(null, true);
-    }
+    return callback(null, true);
+    // if (process.env.NODE_ENV === "development") {
+    //   return callback(null, true);
+    // }
 
-    if (typeof origin === "undefined") {
-      return callback(new Error("Not allowed by CORS"), false);
-    }
+    // if (typeof origin === "undefined") {
+    //   return callback(new Error("Not allowed by CORS"), false);
+    // }
 
-    if (whiteListedUrls.includes(origin)) {
-      return callback(null, true);
-    }
+    // if (whiteListedUrls.includes(origin)) {
+    //   return callback(null, true);
+    // }
 
-    return callback(new Error("Not allowed by CORS"), false);
+    // return callback(new Error("Not allowed by CORS"), false);
   },
   credentials: true,
   exposedHeaders: ["Authorization"],
